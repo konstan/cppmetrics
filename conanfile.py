84f4a5977b56d3a3bfea7224c9b123f91768ea7d
Kyle Maxwell <kyle@kylemaxwell.com>
2016-10-25 15:42:35 +0200

    wip

--- conanfile.py
+++ conanfile.py
@@ -20,8 +20,9 @@ class CppmetricsConan(ConanFile):
         
     def package(self):
         self.copy("*.h", dst="include")
-        self.copy("*.lib", dst="lib", src="lib")
-        self.copy("*.a", dst="lib", src="lib")
+        self.copy("*.lib", dst="lib", src=".")
+        self.copy("*.dylib", dst="lib", src=".")
+        self.copy("*.a", dst="lib", src=".")
 
     def package_info(self):
         self.cpp_info.libs = ["cppmetrics"]
